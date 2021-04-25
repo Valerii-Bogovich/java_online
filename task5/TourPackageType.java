2238e8c07fddca06f13cd429528d1ab565b22b0f
Valerii-Bogovich <51030527+Valerii-Bogovich@users.noreply.github.com>
2021-04-25 18:15:00 +0300

    Delete TourPackageType.java

--- task5/TourPackageType.java
+++
@@ -1,9 +0,0 @@
-package by.epam.jonline.programming_with_classes.agregation_and_composition.task5;
-
-public enum TourPackageType {
-	RECREATION,
-    EXCURSION,
-    TREATMENT,
-    SHOPPING,
-    CRUISE;
-}
